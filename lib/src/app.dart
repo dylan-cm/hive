a8c4522e884439ec0bf465356590deaf235e2e8b
Dylan Modell <dylan.modell.promo@gmail.com>
2019-06-08 02:29:07 -0700

    tap to see moves

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,5 +1,4 @@
 import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:polygon_clipper/polygon_clipper.dart';
 
@@ -16,9 +15,10 @@ class App extends StatelessWidget {
 }
 
 class Hex extends StatelessWidget {
-  const Hex({Key key, this.color}) : super(key: key);
+  const Hex({Key key, this.color, this.elevation}) : super(key: key);
 
   final Color color;
+  final double elevation; 
 
   @override
   Widget build(BuildContext context) {
@@ -31,7 +31,7 @@ class Hex extends StatelessWidget {
           sides: 6, 
           borderRadius: 5.0,
           rotate: 90.0,
-          boxShadows: [ PolygonBoxShadow(color: Colors.black, elevation: 2.0) ],
+          boxShadows: [ PolygonBoxShadow(color: Colors.black, elevation: elevation) ],
           child: Container(color: color),
         ),
       ),
@@ -46,26 +46,65 @@ class Bee extends StatefulWidget {
 
 class _BeeState extends State<Bee> {
   bool movesVisible;
-  List<Hex> moves;
+  List<Widget> moves;
 
   @override
   void initState() {
     super.initState();
     movesVisible = false;
-    
+    moves = [];
   }
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onTap,
-      child: Hex(
-        color: Colors.yellow,
-      ),
+    return Stack(
+      children: <Widget>[
+        Container(
+          alignment: Alignment(0,0),
+          child: GestureDetector(
+            onTap: onTap,
+            child: Hex(
+              color: Colors.yellow,
+              elevation: 0.0,
+            ),
+          ),
+        ),
+      ] + moves,
     );
   }
 
   void onTap(){
-    setState(()=>movesVisible=true);
+    setState((){
+      moves = movesVisible ?calcMoves() :[] ;
+      movesVisible=!movesVisible;
+    });
+  }
+
+  List<Widget> calcMoves(){
+    List<Widget> moves = [];
+    List<dynamic> coords = [
+      {'x':0.0, 'y':-0.5},
+      {'x':0.0, 'y':0.5},
+      {'x':0.75 / sqrt(3) * 2, 'y':0.25},
+      {'x':0.75 / sqrt(3) * 2, 'y':-0.25},
+      {'x':-0.75 / sqrt(3) * 2, 'y':0.25},
+      {'x':-0.75 / sqrt(3) * 2, 'y':-0.25},
+    ];
+    for(var i = 0; i<coords.length; i++){
+      moves.add(
+        Container(
+          alignment: Alignment(coords[i]['x'],coords[i]['y']),
+          child: GestureDetector(
+            onTap: onTap,
+            child: Hex(
+              color: Colors.blueGrey[100],
+              elevation: 0.0,
+            ),
+          ),
+        )
+      );
+    }
+
+    return moves;
   }
 }
